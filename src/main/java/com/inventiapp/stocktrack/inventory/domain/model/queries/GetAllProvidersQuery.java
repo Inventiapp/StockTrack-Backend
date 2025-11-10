40804115659c7b6d5a776b43358e3313f86c2d6c
Mema <patty_60071027@outlook.es>
2025-11-10 16:28:28 -0500

    feat: add get all providers query

---
+++ src/main/java/com/inventiapp/stocktrack/inventory/domain/model/queries/GetAllProvidersQuery.java
@@ -0,0 +1,11 @@
+package com.inventiapp.stocktrack.inventory.domain.model.queries;
+
+/**
+ * GetAllProvidersQuery
+ * @summary
+ * Query object representing a request to retrieve all Provider aggregates.
+ * This simple query is intentionally parameterless; in the future it may be extended
+ * with pagination, filtering or sorting parameters if required.
+ * @since 1.0
+ */
+public record GetAllProvidersQuery() {}
