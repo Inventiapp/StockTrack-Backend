de04a853a3924b6bfff23854b6a7aa97f9c3d71b
Dayro <dayro.rios.p@gmail.com>
2025-11-17 01:39:09 -0500

    refactor(cors): streamline CORS configuration for development and production profiles

--- src/main/java/com/inventiapp/stocktrack/shared/infrastructure/configuration/WebConfig.java
+++ src/main/java/com/inventiapp/stocktrack/shared/infrastructure/configuration/WebConfig.java
@@ -1,20 +1,20 @@
 package com.inventiapp.stocktrack.shared.infrastructure.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Profile("dev")
-public class CorsConfigDev implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD")
                 .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
