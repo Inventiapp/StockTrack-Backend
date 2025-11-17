72fd0799fe600b436672239e6de8cda8dd2289fe
Dayro <dayro.rios.p@gmail.com>
2025-11-17 00:09:12 -0500

    feat(cors): implement separate CORS configurations for development and production profiles

---
+++ src/main/java/com/inventiapp/stocktrack/shared/infrastructure/configuration/CorsConfigDev.java
@@ -0,0 +1,19 @@
+package com.inventiapp.stocktrack.shared.infrastructure.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("dev")
+public class CorsConfigDev implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
--- src/main/java/com/inventiapp/stocktrack/shared/infrastructure/configuration/CorsConfigProd.java
+++ src/main/java/com/inventiapp/stocktrack/shared/infrastructure/configuration/CorsConfigProd.java
@@ -3,15 +3,18 @@ package com.inventiapp.stocktrack.shared.infrastructure.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-public class CorsConfig {
+@Profile("prod")
+public class CorsConfigProd implements WebMvcConfigurer {
 
     @Bean
     public CorsFilter corsFilter() {
