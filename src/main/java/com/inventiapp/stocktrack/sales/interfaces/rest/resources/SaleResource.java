329e4cbe74c711173cb9d6e171f95d3d65ac8bac
Dayro <dayro.rios.p@gmail.com>
2025-12-04 18:11:18 -0500

    feat(sales): add createdAt field to SaleResource and update assembler

--- src/main/java/com/inventiapp/stocktrack/sales/interfaces/rest/resources/SaleResource.java
+++ src/main/java/com/inventiapp/stocktrack/sales/interfaces/rest/resources/SaleResource.java
@@ -1,7 +1,8 @@
 package com.inventiapp.stocktrack.sales.interfaces.rest.resources;
 
 
+import java.util.Date;
 import java.util.List;
 
-public record SaleResource(Long id, double totalAmount, List<SaleDetailResource> details) {
+public record SaleResource(Long id, double totalAmount, Date createdAt, List<SaleDetailResource> details) {
 }
