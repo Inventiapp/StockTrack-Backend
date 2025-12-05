b0572ebde041bc394a56ee2b8665f7b9f6954709
Piero Sulca <psulcasanchez@gmail.com>
2025-12-05 12:10:24 -0500

    refactor(iam): remove unused queries and roles controller

--- src/main/java/com/inventiapp/stocktrack/iam/domain/model/queries/GetUserByEmailQuery.java
+++
@@ -1,9 +0,0 @@
-package com.inventiapp.stocktrack.iam.domain.model.queries;
-
-/**
- * Query to get a user by email
- * @param email The email
- */
-public record GetUserByEmailQuery(String email) {
-}
-
